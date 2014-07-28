744392f3f70f608732fa3f7e26d39584e2e8a9a1
Fab Mackojc <fab.mackojc@gmail.com>
2014-07-27 21:53:56 -0700

    Use TZInfo to display date in origin's local timezone

--- app.rb
+++ app.rb
@@ -1,4 +1,13 @@
 require 'twitter'
+require 'tzinfo'
+
+timezone_hash = {}
+
+f = File.open('timezone_map.txt')
+f.each_line do |line|
+  airport_code, timezone_name = line.strip.split(/\t/)
+  timezone_hash[airport_code] = timezone_name
+end
 
 client = Twitter::REST::Client.new do |config|
   config.consumer_key        = ENV['TWITTER_KEY']
@@ -12,7 +21,10 @@ client.user_timeline('fabsays', {count: 200}).each do |tweet|
   tweet_id = tweet.id
   if /-&gt/ === tweet_text  # check if tweet text contains '->'
     origin, destination = tweet_text.scan(/\w\w\w/)
-    tweet_date = tweet.created_at.strftime('%b %d %Y')
+    timezone_name = timezone_hash[origin]
+    timezone = TZInfo::Timezone.get(timezone_name)
+    tweet_date_utc = tweet.created_at
+    tweet_date = timezone.utc_to_local(tweet_date_utc).strftime('%b %d %Y')
     puts "You flew from #{origin} to #{destination} on #{tweet_date}"
   end
 end

99e24a31c1ca65c1fb66d94a61835b53edf10b94
Fab Mackojc <fab.mackojc@gmail.com>
2015-02-11 10:46:20 -0800

    Code now pages past first 200 tweets to see all my flight tweets

--- app.rb
+++ app.rb
@@ -16,15 +16,50 @@ client = Twitter::REST::Client.new do |config|
   config.access_token_secret = ENV['TWITTER_TOKEN_SECRET']
 end
 
+all_tweets = []
+
 client.user_timeline('fabsays', {count: 200}).each do |tweet|
   tweet_text = tweet.text
-  tweet_id = tweet.id
   if /-&gt/ === tweet_text  # check if tweet text contains '->'
     origin, destination = tweet_text.scan(/\w\w\w/)
     timezone_name = timezone_hash[origin]
     timezone = TZInfo::Timezone.get(timezone_name)
     tweet_date_utc = tweet.created_at
     tweet_date = timezone.utc_to_local(tweet_date_utc).strftime('%b %d %Y')
-    puts "You flew from #{origin} to #{destination} on #{tweet_date}"
+    tweet_id = tweet.id
+    tweet_hash = {id: tweet_id, origin: origin, destination: destination, date: tweet_date}
+    all_tweets << tweet_hash
   end
 end
+
+# Now do paging to see past first 200 tweets
+
+while true
+  tweets_on_this_request = []
+  max_id = all_tweets.last[:id] - 1
+
+  client.user_timeline('fabsays', {count: 200, max_id: max_id}).each do |tweet|
+    tweet_text = tweet.text
+    if /-&gt/ === tweet_text  # check if tweet text contains '->'
+      origin, destination = tweet_text.scan(/\w\w\w/)
+      timezone_name = timezone_hash[origin]
+      timezone = TZInfo::Timezone.get(timezone_name)
+      tweet_date_utc = tweet.created_at
+      tweet_date = timezone.utc_to_local(tweet_date_utc).strftime('%b %d %Y')
+      tweet_id = tweet.id
+      tweet_hash = {id: tweet_id, origin: origin, destination: destination, date: tweet_date}
+      tweets_on_this_request << tweet_hash
+    end
+  end
+
+  break if tweets_on_this_request.length == 0
+
+  all_tweets.concat(tweets_on_this_request)
+end
+
+all_tweets.each do |tweet|
+  puts "Tweet ID: #{tweet[:id]}"
+  puts "You flew from #{tweet[:origin]} to #{tweet[:destination]} on #{tweet[:date]}"
+end
+
+puts "Total Flights: #{all_tweets.length}"
